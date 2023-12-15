afd58ad0fb46200ccad61612397835cac60eecee
Matias-Ibarra <89396537+Matias-Ibarra@users.noreply.github.com>
2023-12-15 23:40:49 +0100

    15/12 matias

--- matias.py
+++ matias.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from langchain.chains import AnalyzeDocumentChain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
-import sqlite3
+import pymysql
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,25 +11,36 @@ llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key="sk-BBAkv90HMoifNClpHyO0T
 qa_chain = load_qa_chain(llm, chain_type="map_reduce")
 qa_document_chain = AnalyzeDocumentChain(combine_docs_chain=qa_chain)
 
-def guardar_en_base_de_datos(consulta, respuesta):
-    # Nombre del archivo de la base de datos
-    db_file = 'bbdd_pruebas.db'
+def guardar_en_base_de_datos(consulta, respuesta, archivo_nombre):
+    # Información de conexión a la base de datos de AWS
+    username = "admin"
+    password = "elgrupo4"
+    host = "database-2.cuwmrnsvir86.eu-north-1.rds.amazonaws.com"
+    port = 3306
+    database = "database-2"  # Reemplaza con el nombre de tu base de datos
 
-    # Crear una conexión a la base de datos (esto también crea el archivo si no existe)
-    conn = sqlite3.connect(db_file)
+    # Conectar a la base de datos
+    connection = pymysql.connect(
+        host=host,
+        user=username,
+        password=password,
+        database=database,
+        port=port,
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
     # Crear un cursor para ejecutar comandos SQL
-    cursor = conn.cursor()
+    cursor = connection.cursor()
 
     try:
         # Comando SQL para insertar datos en la tabla
-        insert_data_query = 'INSERT INTO consultas_respuestas (consulta, respuesta) VALUES (?, ?)'
+        insert_data_query = 'INSERT INTO consultas (consulta, respuesta, archivo) VALUES (%s, %s, %s)'
 
         # Ejecutar el comando SQL para insertar los datos
-        cursor.execute(insert_data_query, (consulta, respuesta))
+        cursor.execute(insert_data_query, (consulta, respuesta, archivo_nombre))
 
         # Confirmar la inserción de datos
-        conn.commit()
+        connection.commit()
 
         print("Datos insertados en la base de datos.")
     except Exception as e:
@@ -38,7 +49,7 @@ def guardar_en_base_de_datos(consulta, respuesta):
     finally:
         # Cerrar el cursor y la conexión
         cursor.close()
-        conn.close()
+        connection.close()
 
 @app.route('/')
 def index():
@@ -46,16 +57,26 @@ def index():
 
 @app.route('/api/query', methods=['POST'])
 def query():
-    document = request.files['document'].read().decode('utf-8')
+    document = request.files['document']
     question = request.form['question']
 
+    # Almacenar el archivo subido por el usuario
+    if document:
+        archivo_nombre = document.filename
+        document_content = document.read().decode('utf-8')  # Leer el contenido del archivo
+    else:
+        archivo_nombre = None
+        document_content = None
+
     # Llamada a Langchain
-    response = qa_document_chain.run(input_document=document, question=question)
+    response = qa_document_chain.run(input_document=document_content, question=question)
+    print(response)
 
     # Almacenar en la base de datos
-    guardar_en_base_de_datos(question, response)
-
-    return jsonify({'answer': response, 'question': question})
+    guardar_en_base_de_datos(question, response, archivo_nombre)
+    print({'consulta': question, 'archivo': archivo_nombre, 'respuesta': response})
+    return jsonify({'answer': response})
+    # return jsonify({'consulta': question, 'archivo': archivo_nombre, 'respuesta': response})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
